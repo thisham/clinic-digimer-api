e58e0b114f075c81a4cd0ac3a10ff77a89930ae1
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-05-09 18:28:43 +0700

    chore: add migrator for medical record categories

--- src/database/mysql.go
+++ src/database/mysql.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	medicalRecordCategories "digimer-api/src/app/medical_record_categories/repositories"
 	medicines "digimer-api/src/app/medicines/repositories"
 	polyclinics "digimer-api/src/app/polyclinics/repositories"
 	"digimer-api/src/configs"
@@ -20,9 +21,9 @@ func (DB *DBConf) InitDB() *DBConf {
 }
 
 func (DB *DBConf) Migrate() {
-	DB.AutoMigrate(&polyclinics.Polyclinic{}, &medicines.Medicine{})
+	DB.AutoMigrate(&polyclinics.Polyclinic{}, &medicines.Medicine{}, &medicalRecordCategories.MedicalRecordCategory{})
 }
 
 func (DB *DBConf) Demigrate() {
-	DB.Migrator().DropTable(&polyclinics.Polyclinic{}, &medicines.Medicine{})
+	DB.Migrator().DropTable(&polyclinics.Polyclinic{}, &medicines.Medicine{}, &medicalRecordCategories.MedicalRecordCategory{})
 }
