adc75bb28430bf8515a740472fbefa23f38744b1
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-05-11 05:27:35 +0700

    feat(patients): implement usecases

--- src/app/patients/services/usecases.go
+++ src/app/patients/services/usecases.go
@@ -1,6 +1,12 @@
 package services
 
-import "digimer-api/src/app/patients"
+import (
+	"digimer-api/src/app/patients"
+	errormessages "digimer-api/src/constants/error_messages"
+	"errors"
+
+	"github.com/google/uuid"
+)
 
 type usecase struct {
 	repo patients.Repositories
@@ -8,37 +14,48 @@ type usecase struct {
 
 // AmendPatientByID implements patients.Services
 func (uc *usecase) AmendPatientByID(id string, domain patients.Domain) (err error) {
-	panic("unimplemented")
+	isFound := uc.repo.CountDataByID(id) != 0
+
+	if isFound {
+		return uc.repo.UpdateByID(id, domain)
+	}
+	return errors.New(errormessages.FoundNoData)
 }
 
 // CountPatientByID implements patients.Services
 func (uc *usecase) CountPatientByID(id string) (count int) {
-	panic("unimplemented")
+	return uc.repo.CountDataByID(id)
 }
 
 // CreatePatient implements patients.Services
 func (uc *usecase) CreatePatient(domain patients.Domain) (id string, err error) {
-	panic("unimplemented")
+	domain.ID = uuid.New()
+	return domain.ID.String(), uc.repo.InsertData(domain)
 }
 
 // GetAllPatients implements patients.Services
 func (uc *usecase) GetAllPatients() (patients []patients.Domain, err error) {
-	panic("unimplemented")
+	return uc.repo.SelectAllData()
 }
 
 // GetPatientByID implements patients.Services
 func (uc *usecase) GetPatientByID(id string) (patient patients.Domain, err error) {
-	panic("unimplemented")
+	return uc.repo.SelectDataByID(id)
 }
 
 // GetPatientByMRBookNumber implements patients.Services
 func (uc *usecase) GetPatientByMRBookNumber(mrBookNumber string) (patient patients.Domain, err error) {
-	panic("unimplemented")
+	return uc.repo.SelectDataByMRBookNumber(mrBookNumber)
 }
 
 // RemovePatientByID implements patients.Services
 func (uc *usecase) RemovePatientByID(id string) (err error) {
-	panic("unimplemented")
+	isFound := uc.repo.CountDataByID(id) != 0
+
+	if isFound {
+		return uc.repo.DeleteByID(id)
+	}
+	return errors.New(errormessages.FoundNoData)
 }
 
 func NewService(repo patients.Repositories) patients.Services {
